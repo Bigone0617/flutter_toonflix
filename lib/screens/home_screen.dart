99692128815fcc00cecf15a7a43c4d3a16435111
Bigone0617 <xodlfsha@gmail.com>
2024-10-21 02:08:31 +0900

    list view and non stateWidget is fetching data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,35 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix_app/models/webtoon_model.dart';
+import 'package:toonflix_app/services/api_service.dart';
 
-import '../models/webtoon_model.dart';
-import '../services/api_service.dart';
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebtoons() async {
-    webtoons = await ApiService.getTodayWebtoon();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    waitForWebtoons();
-  }
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodayWebtoon();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
       appBar: AppBar(
         foregroundColor: Colors.green,
@@ -38,6 +17,25 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(fontSize: 24),
         ),
         backgroundColor: Colors.white,
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.separated(
+              scrollDirection: Axis.vertical,
+              itemCount: snapshot.data!.length,
+              separatorBuilder: (context, index) => const SizedBox(height: 40),
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
